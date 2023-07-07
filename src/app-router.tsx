a4b9ad995e4c89ae5d41f4481db6383b11927138
Ruisi Lu <lhc03952@gmail.com>
2023-07-07 20:22:11 +0800

    feat: add app router and hompage

---
+++ src/app-router.tsx
@@ -0,0 +1,10 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
+const AppRoutes: React.FC = () => (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+);
+
+export default AppRoutes;
