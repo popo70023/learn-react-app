9591cd4e91e6eb6329af19e35cdabd5e6dd4834c
Ruisi Lu <lhc03952@gmail.com>
2023-07-15 13:32:52 +0800

    chore: add generate-react-cli template for react17+

---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,12 @@
+import { type FC } from 'react';
+import './HorizonalMenu.scss';
+
+interface HorizonalMenuProps {}
+
+const HorizonalMenu: FC<HorizonalMenuProps> = () => (
+  <div className="HorizonalMenu" data-testid="HorizonalMenu">
+    HorizonalMenu Component
+  </div>
+);
+
+export default HorizonalMenu;
