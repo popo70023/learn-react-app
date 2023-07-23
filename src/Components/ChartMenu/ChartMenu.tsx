4ac5c12d79c056fb19c77499e7753d87310b9706
ffaa0100aa <ffaa0100aa@gmail.com>
2023-07-23 21:54:31 +0800

    fix: fix childs display problem and vertical-line display position problem

--- src/Components/ChartMenu/ChartMenu.tsx
+++ src/Components/ChartMenu/ChartMenu.tsx
@@ -1,59 +1,61 @@
 import type React from 'react';
-import  { useState } from 'react';
+import { useState } from 'react';
 import './ChartMenu.scss';
 import { Link } from 'react-router-dom';
 
-interface ChartMenuProps {}
-interface MenuButton{
+interface ChartMenuProps { }
+interface MenuButtonLink{
   name: string;
   link: string;
-  childText?: string[];
+}
+interface MenuButton {
+  menuButtonLink: MenuButtonLink;
+  childs?: MenuButtonLink[];
   className?: string;
 }
-
 const ChartMenu: React.FC<ChartMenuProps> = () => {
   const menuButtons: MenuButton[] = [
-  { name: 'All-Payments', link: '/', childText: ['All-transactions'] },
-  { name: 'Fraud-Risk', link: '/' },
-  { name: 'Invoices', link: '/' },
-  { name: 'Subscriptions', link: '/' },
-  { name: 'Quotes', link: '/' },
-  { name: 'Payment-Link', link: '/',className: 'payment-link-btn'  },
-    ];
-  const [activeButton, setActiveButton] = useState('Home');
+    { menuButtonLink: {name: 'All-Payments', link: '/'}, childs: [{name:'All-transactions', link: '/'}, {name:'132', link: '/'}, {name:'acvav', link: '/'}] },
+    { menuButtonLink: {name:'Fraud-Risk', link: '/'} },
+    { menuButtonLink: {name:'Invoices', link: '/'} },
+    { menuButtonLink: {name:'Subscriptions', link: '/'} },
+    { menuButtonLink: {name:'Quotes', link: '/'} },
+    { menuButtonLink: {name:'Payment-Link', link: '/'}, className: 'payment-link-btn' },
+  ];
+  const [activeButton, setActiveButton] = useState<string | null>(null);
   const [showChildText, setShowChildText] = useState(false);
   const onButtonClick = (buttonName: string, childText?: string[]) => {
-    setActiveButton(buttonName);
+    setActiveButton((prevActiveButton) => (prevActiveButton === buttonName ? null : buttonName));
     setShowChildText(childText !== undefined && childText.length > 0);
   };
-    return(
-      <div className="ChartMenu" data-testid="ChartMenu">
+  return (
+    <div className="ChartMenu" data-testid="ChartMenu">
       <span className="Tittle">Payments</span>
-       {menuButtons.map((button, index) => (
-        <Link
-          key={index}
-          className="Chart-menu-link"
-          to={button.link}
-          onClick={() => { onButtonClick(button.name); }}
-          
-        >
-          <div className={`Chart-menu-btn  ${activeButton === button.name? 'Chart-menu-btn-active': ''}`}>{button.name}
- 
-          </div>
-        </Link>
-      ))} 
-      {showChildText && (
-        <div className="childTextContainer">
-          {menuButtons.find((button) => button.name === 'All-Payments')?.childText?.map((text, index) => (
-            <p key={index} className="childText">
-              {text}
-            </p>
-          ))}
+      {menuButtons.map((button, index) => (
+        <div key={index}>
+          <Link
+            
+            className="Chart-menu-link"
+            to={button.menuButtonLink.link}
+            onClick={() => { onButtonClick(button.menuButtonLink.name); }}
+          >
+            <div className={`Chart-menu-btn  ${activeButton === button.menuButtonLink.name ? 'Chart-menu-btn-active' : ''}`}>
+              <div className={`vertical-line ${activeButton === button.menuButtonLink.name ?'vertical-line-active':'' }`}></div>
+              {button.menuButtonLink.name} </div>
+          </Link>
+          {button.childs !== undefined &&activeButton === button.menuButtonLink.name ? button.childs.map((child, childTextIndex) => (
+            <Link
+            key={childTextIndex}
+            className='child-text'
+            to={child.link}
+            >{child.name}
+            </Link>
+          )) : null}
         </div>
-      )}
+      ))}
       <div className="backgroundLine"></div>
     </div>
-     );
- 
+  );
+
 };
 export default ChartMenu;
