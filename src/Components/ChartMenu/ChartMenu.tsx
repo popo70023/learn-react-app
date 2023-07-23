bf02ee21891df3bdf350d2903dcc4efe4d2b5f5e
ffaa0100aa <ffaa0100aa@gmail.com>
2023-07-23 21:21:12 +0800

    feat: add ChartMenu ui

---
+++ src/Components/ChartMenu/ChartMenu.tsx
@@ -0,0 +1,77 @@
+import type React from 'react';
+import  { useState } from 'react';
+import './ChartMenu.scss';
+interface ChartMenuProps{};
+const ChartMenu: React.FC<ChartMenuProps> = () => {
+
+    return (
+        <div className="rectangle"> 
+        <span className="Payments">Payments</span>
+        
+        <ParentText text="All-Payments">
+          <ChildText text="All-transactions" />
+        </ParentText>
+        <ParentText text="Fraud-Risk">
+          
+        </ParentText>
+        <ParentText text="Invoices">
+          
+        </ParentText>
+        <ParentText text="Subscriptions">
+      
+        </ParentText>
+        <ParentText text="Quotes">
+      
+        </ParentText>
+        <ParentText text="Payment-Link" className="last-parent-text">
+
+        </ParentText>
+      </div> 
+      );
+};
+interface ParentTextProps {
+    text: string;
+    children: React.ReactNode;
+    className?: string;
+  };
+  
+  const ParentText: React.FC<ParentTextProps> = ({ text, children, className }) => {
+    const [showChildren, setShowChildren] = useState(false);
+    const [clicked, setClicked] = useState(false);
+  
+    const handleClick = () => {
+      setShowChildren(!showChildren);
+      setClicked(!clicked);
+    };
+  
+    return (
+      <div className={`parent-text-container ${className ?? ''}`}>
+        <div className={`line${clicked ? ' clicked' : ''}`}></div>
+        <div className={'backgroundLine'}></div>
+        <p className={`parent-text${clicked ? ' clicked' : ''}`} onClick={handleClick}>
+          {text}
+        </p>
+        {showChildren && <div className="child-container">{children}</div>}
+      </div>
+    );
+  };
+  
+  interface ChildTextProps {
+    text: string;
+  }
+  
+  const ChildText: React.FC<ChildTextProps> = ({ text }) => {
+    const [childClicked, setChildClicked] = useState(false);
+  
+    const handleChildClick = () => {
+      setChildClicked(!childClicked);
+    };
+  
+    return (
+      <p className={`child-text${childClicked ? ' clicked' : ''}`} onClick={handleChildClick}>
+        {text}
+      </p>
+    );
+  };
+  
+export default ChartMenu;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
 import { type FC } from 'react';
 import './HomePage.scss';
-
+import ChartMenu from '../../Components/ChartMenu/ChartMenu';
 interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => (
   <div className="HomePage" data-testid="HomePage">
-    HomePage Component
+    <ChartMenu/>
   </div>
 );
 
