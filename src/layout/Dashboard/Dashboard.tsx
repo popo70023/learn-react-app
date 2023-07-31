d3542c94b2ec587442d99c2e722ec541c4ac6545
Olive020 <a0977062020@gmail.com>
2023-07-31 17:09:39 +0800

    feat: modify dashboard style

--- src/layout/Dashboard/Dashboard.tsx
+++ src/layout/Dashboard/Dashboard.tsx
@@ -6,7 +6,9 @@ import HorizontalMenu from '../../Components/HorizontalMenu/HorizontalMenu';
 interface DashboardProps {}
 const Dashboard: FC<DashboardProps> = () => (
   <div className="Dashboard" data-testid="Dashboard">
+    <div className="header">
     <Header />
+    </div>
     <HorizontalMenu />
   </div>
 );
