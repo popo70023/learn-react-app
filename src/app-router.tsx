7743c037b20633c0184c98e439305355a87c3c16
Olive020 <a0977062020@gmail.com>
2023-08-12 16:42:56 +0800

    feat: modify index style to body

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/HomePage/HomePage";
 import PaymentsPage from "./pages/PaymentsPage/PaymentsPage";
 import PaymentsList from "./components/PaymentsList/PaymentsList";
 import ChartMenu from "./components/ChartMenu/ChartMenu";
-import Api from "./pages/Api/Api";
 const AppRoutes: React.FC = () => (
      <Routes>
         <Route path="/" element={<HomePage/>} />
