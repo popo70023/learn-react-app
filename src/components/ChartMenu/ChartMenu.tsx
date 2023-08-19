bc5d7e51490208efd9dfd3c3a8cb122223398b3e
ffaa0100aa <ffaa0100aa@gmail.com>
2023-08-19 21:18:15 +0800

    feat: modify rwd ui

--- src/components/ChartMenu/ChartMenu.tsx
+++ src/components/ChartMenu/ChartMenu.tsx
@@ -18,8 +18,8 @@ const ChartMenu: React.FC<ChartMenuProps> = () => {
     { menuButtonLink: { name: 'All-Payments', link: '/Payments' }, childs: [{ name: 'All-transactions', link: '/Payments/All-transactions' }] },
     { menuButtonLink: { name: 'Fraud-Risk', link: '/Payments' } },
     { menuButtonLink: { name: 'Invoices', link: '/Payments' } },
-    { menuButtonLink: { name: 'Subscriptions  ', link: '/Payments' }},
-    { menuButtonLink: { name: 'Quotes', link: '/Payments' }},
+    { menuButtonLink: { name: 'Subscriptions  ', link: '/Payments' } },
+    { menuButtonLink: { name: 'Quotes', link: '/Payments' } },
     { menuButtonLink: { name: 'Payment-Link', link: '/Payments' } },
   ];
   const [activeButton, setActiveButton] = useState<string | null>(null);
@@ -29,101 +29,102 @@ const ChartMenu: React.FC<ChartMenuProps> = () => {
     setShowChildText(childText !== undefined && childText.length > 0);
   };
 
-  
+
 
   return (
     <div className='page'>
       <div className="ChartMenu" data-testid="ChartMenu">
-       
-          <div className="vertical-menu">
-            <span className="Tittle">Payments</span>
-            <div className='Menu-Container'>
 
-              {menuButtons.map((button, index) => (
-                <div key={index} className='test'>
+        <div className="vertical-menu">
+          <span className="Tittle">Payments</span>
+          <div className='Menu-Container'>
+
+            {menuButtons.map((button, index) => (
+              <div key={index} className='test'>
 
 
+                <Link
+                  className="Chart-menu-link"
+                  to={button.menuButtonLink.link}
+                  onClick={() => { onButtonClick(button.menuButtonLink.name); }}
+                >
+                  <div className={`Chart-menu-btn   ${activeButton === button.menuButtonLink.name ? 'Chart-menu-btn-active' : ''}`}>
+                    <div className={`vertical-line ${activeButton === button.menuButtonLink.name ? 'vertical-line-active' : ''}`}></div>
+                    {button.menuButtonLink.name}
+                  </div>
+                </Link>
+
+                {button.childs !== undefined && activeButton === button.menuButtonLink.name ? button.childs.map((child, childTextIndex) => (
                   <Link
-                    className="Chart-menu-link"
-                    to={button.menuButtonLink.link}
-                    onClick={() => { onButtonClick(button.menuButtonLink.name); }}
-                  >
-                    <div className={`Chart-menu-btn   ${activeButton === button.menuButtonLink.name ? 'Chart-menu-btn-active' : ''}`}>
-                      <div className={`vertical-line ${activeButton === button.menuButtonLink.name ? 'vertical-line-active' : ''}`}></div>
-                      {button.menuButtonLink.name}
-                    </div>
+                    key={childTextIndex}
+                    className='child-text'
+                    to={child.link}
+                  >{child.name}
                   </Link>
+                )) : null}
 
-                  {button.childs !== undefined && activeButton === button.menuButtonLink.name ? button.childs.map((child, childTextIndex) => (
-                    <Link
-                      key={childTextIndex}
-                      className='child-text'
-                      to={child.link}
-                    >{child.name}
-                    </Link>
-                  )) : null}
-
-                </div>
-              ))}
-              <div className="backgroundLine"></div>
-            </div>
-          </div>
-      
-          <div className="horizontal-menu">
-              <div className='Menu-Container'>
-
-                {menuButtons.map((button, index) => (
-                  <div key={index} className='test'>
-                    <Link
-                      className="Chart-menu-link"
-                      to={button.menuButtonLink.link}
-                      onClick={() => { onButtonClick(button.menuButtonLink.name); }}
-                    >
-                      <div className={`Chart-menu-btn   ${activeButton === button.menuButtonLink.name ? 'Chart-menu-btn-active' : ''}`}>
-                     
-                        {button.menuButtonLink.name}
-                      </div>
-                    </Link>
-                    
-                  </div>
-                  
-                ))}
-              
               </div>
-               
-  
+            ))}
+            <div className="backgroundLine"></div>
           </div>
-          
-          
-        
-      </div>
-  <div className='child-contianer'>
-                
-             {menuButtons.map((button, index) => ( 
-                
-                  <div key={index} className='child-menu'>
-                    
-                    {button.childs !== undefined && activeButton === button.menuButtonLink.name ? button.childs.map((child, childTextIndex) => (
-                        <Link
-                          key={childTextIndex}
-                          className='child-text'
-                          to={child.link}
-                        >{child.name}
-                        </Link>
-                      )) : null}
-                       
+        </div>
+
+        <div className="horizontal-menu">
+          <div className='Menu-Container'>
+
+            {menuButtons.map((button, index) => (
+              <div key={index} className='test'>
+                <Link
+                  className="Chart-menu-link"
+                  to={button.menuButtonLink.link}
+                  onClick={() => { onButtonClick(button.menuButtonLink.name); }}
+                >
+                  <div className={`Chart-menu-btn   ${activeButton === button.menuButtonLink.name ? 'Chart-menu-btn-active' : ''}`}>
+
+                    {button.menuButtonLink.name}
                   </div>
-               
-                ))}
-                  
-                </div>
-             {activeButton && (
-                  <div className="triangle" ></div>
-                )}
+                </Link>
+
+              </div>
+
+            ))}
+
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+      <div className='child-contianer'>
+
+        {menuButtons.map((button, index) => (
+
+          <div key={index} className='child-menu'>
+
+            {button.childs !== undefined && activeButton === button.menuButtonLink.name ? button.childs.map((child, childTextIndex) => (
+              <Link
+                key={childTextIndex}
+                className='child-text'
+                to={child.link}
+              >{child.name}
+              </Link>
+            )) : null}
+
+          </div>
+
+        ))}
+
+      </div>
+      {activeButton !== null && activeButton !== undefined && (
+        <div className="triangle"></div>
+      )}
+
       <Routes>
         <Route path='/All-transactions' element={<div className='PaymentsPage'><PaymentsPage /></div>} />
       </Routes>
-      
+
     </div>
   );
 };
