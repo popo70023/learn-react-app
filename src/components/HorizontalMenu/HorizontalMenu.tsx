ff8c9195369b7711df029423f434652d14b01501
Olive020 <a0977062020@gmail.com>
2023-08-22 16:04:11 +0800

    feat: modify dashboard style to position

--- src/components/HorizontalMenu/HorizontalMenu.tsx
+++ src/components/HorizontalMenu/HorizontalMenu.tsx
@@ -83,11 +83,11 @@ const HorizontalMenu: FC<HorizontalMenuProps> = () => {
               </li>
             </ul>
           </nav>
-          <svg className={`dropdown-svg  ${showAboutMenu ? 'dropdown-svg-active' : ''}`}  xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" fill="none">
+          <svg className={`dropdown-svg  ${showAboutMenu ? 'dropdown-svg-active' : ''}`} xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" fill="none">
             <path d="M1 1L5 5L9 1" stroke="#0C1E5B" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
           <button className="horizontal-menu-link developers"><div className="horizontal-menu-btn">{'Developers'}</div></button>
-          <p className="horizontal-menu-link test-mode"><div className="horizontal-menu-btn">{'Test Mode'}</div></p>
+          <p className=" test-mode">{'Test Mode'}</p>
           <FormControlLabel
             control={<Switch checked={checked} onChange={handleChange} className="switch-button" />} label="" />
         </div>
@@ -100,9 +100,9 @@ const HorizontalMenu: FC<HorizontalMenuProps> = () => {
       {isClicked && <HorizontalMenuResponsive
         handleButtonClick={handleButtonClick}
         activeButton={activeButton} onButtonClick={onButtonClick}
-        showAboutMenu={showAboutMenu} 
-        handleLeave={handleLeave} handleHover={handleHover} 
-        checked={checked} handleChange={handleChange}/>}
+        showAboutMenu={showAboutMenu}
+        handleLeave={handleLeave} handleHover={handleHover}
+        checked={checked} handleChange={handleChange} />}
     </div>
   );
 };
